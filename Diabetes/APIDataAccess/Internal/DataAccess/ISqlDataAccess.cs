9b59f1f6c62afb2daa4e598539922d6d6edeb267
Theodor Risager <theodor349@gmail.com>
2020-11-16 13:08:14 +0100

    Sql DataAccess Files Added

---
+++ Diabetes/APIDataAccess/Internal/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace APIDataAccess.Internal.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        void CommitTransaction();
+        void DeleteData<T>(string storedProcedure, T parameters, string connectionStringName);
+        void DeleteDataInTransaction<T>(string storedProcedure, T parameters);
+        void Dispose();
+        string GetConnectionString(string name);
+        List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
+        List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters);
+        void RollbackTransaction();
+        void SaveData<T>(string storedProcedure, T parameters, string connectionStringName);
+        void SaveDataInTransaction<T>(string storedProcedure, T parameters);
+        void StartTransaction(string connectionStringName);
+    }
+}
---
+++ Diabetes/APIDataAccess/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,132 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace APIDataAccess.Internal.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger<SqlDataAccess> _logger;
+
+        public SqlDataAccess(IConfiguration config, ILogger<SqlDataAccess> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        public string GetConnectionString(string name)
+        {
+            return _config.GetConnectionString(name);
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                var rows = cnn.Query<T>(
+                    storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Execute(
+                    storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public void DeleteData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Execute(
+                    storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+        private bool _isClosed = false;
+
+        public void StartTransaction(string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+            _transaction = _connection.BeginTransaction();
+            _isClosed = false;
+        }
+
+        public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+        {
+            var rows = _connection.Query<T>(
+                storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+            return rows;
+        }
+
+        public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+        {
+            _connection.Execute(
+                storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+
+        public void DeleteDataInTransaction<T>(string storedProcedure, T parameters)
+        {
+            _connection.Execute(
+                storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+
+        public void CommitTransaction()
+        {
+            _transaction?.Commit();
+            _connection?.Close();
+            _isClosed = true;
+        }
+
+        public void RollbackTransaction()
+        {
+            _transaction?.Rollback();
+            _connection?.Close();
+            _isClosed = true;
+        }
+
+        public void Dispose()
+        {
+            if (!_isClosed)
+            {
+                try
+                {
+                    CommitTransaction();
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Commit transaction fail in the dispose method.");
+                }
+            }
+            _connection = null;
+            _transaction = null;
+        }
+    }
+}
