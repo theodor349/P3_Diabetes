76df05dfe1a189ffbbfb90b054803173b498245f
Theodor Risager <theodor349@gmail.com>
2020-11-19 15:25:43 +0100

    Made Models

---
+++ Diabetes/PWA/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWA.Models
+{
+    public class Account
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string NSLink { get; set; }
+        public bool IsEUMeasure { get; set; }
+    }
+}
