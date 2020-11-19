12f61d35e34160cc00122d25ecc87c5ac5f04deb
Bob <54476467+BobbyMcdonald@users.noreply.github.com>
2020-11-19 08:55:04 +0100

    Test

--- Diabetes/APIDataAccess/DataAccess/AccountAccess.cs
+++ Diabetes/APIDataAccess/DataAccess/AccountAccess.cs
@@ -8,6 +8,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
+// Frederik wrote this
 namespace APIDataAccess.DataAccess
 {
     public class AccountAccess : IAccountAccess
