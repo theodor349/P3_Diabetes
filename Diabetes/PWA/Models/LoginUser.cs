99bec1496b833976ae00487f22515032e7a3465a
Theodor Risager <theodor349@gmail.com>
2020-11-20 14:54:05 +0100

    Login Data Flow Setup

---
+++ Diabetes/PWA/Models/LoginUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWA.Models
+{
+    public class LoginUser
+    {
+        public string UserID { get; set; }
+        public string Token { get; set; }
+    }
+}
