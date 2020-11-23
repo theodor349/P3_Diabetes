aefc4a747602ade576484d1ab74251e7445a46c4
Theodor Risager <theodor349@gmail.com>
2020-11-23 15:08:54 +0100

    Notification PopUp works

--- Diabetes/PWA/Models/NotificationData.cs
+++ Diabetes/PWA/Models/NotificationData.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 namespace PWA.Models
 {
     public enum NotificationType { Message, Warning }
+    public enum NotificationButtonType { Snooze, Dismiss}
     public class NotificationData
     {
+        public string Name { get; set; }
         public NotificationType Type { get; set; }
         public string Mesage { get; set; }
     }
