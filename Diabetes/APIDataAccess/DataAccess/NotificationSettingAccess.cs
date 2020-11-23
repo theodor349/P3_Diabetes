44436396fa7eddc0feb25a3f64c253745cb900ed
Gajhede <Hrgajhede@gmail.com>
2020-11-23 10:55:10 +0100

    fandme ja notificicationdataaccess get bby

--- Diabetes/APIDataAccess/DataAccess/NotificationSettingAccess.cs
+++ Diabetes/APIDataAccess/DataAccess/NotificationSettingAccess.cs
@@ -2,11 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using APIDataAccess.Internal.DataAccess;
 
 namespace APIDataAccess.DataAccess
 {
     public class NotificationSettingAccess : INotificationSettingAccess
     {
+        private readonly ISqlDataAccess _sqlDataAccess;
+
+        public NotificationSettingAccess(ISqlDataAccess sqlDataAccess)
+        {
+            _sqlDataAccess = sqlDataAccess;
+        }
+
+
         public void Create(CreateNotificationSettingModel model)
         {
             throw new NotImplementedException();
@@ -14,7 +23,8 @@ namespace APIDataAccess.DataAccess
 
         public List<NotificationSettingModel> Get(string userId)
         {
-            throw new NotImplementedException();
+            var data = _sqlDataAccess.LoadData<NotificationSettingModel, dynamic>("spNotificationSetting_GetByUser", new { UserId = userId }, "DDB");
+            return data;
         }
 
         public void Update(UpdateNotificationSettingModel model)
