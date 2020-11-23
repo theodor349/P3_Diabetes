ae2f1c23f4b50bd84123c85d68612aed0c2555f8
Theodor Risager <theodor349@gmail.com>
2020-11-23 13:02:36 +0100

    Notification

---
+++ Diabetes/PWA/Models/NotificationData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWA.Models
+{
+    public enum NotificationType { Message, Warning }
+    public class NotificationData
+    {
+        public NotificationType Type { get; set; }
+        public string Mesage { get; set; }
+    }
+}
