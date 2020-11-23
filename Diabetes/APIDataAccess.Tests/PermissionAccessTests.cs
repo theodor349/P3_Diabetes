a81fb49f8250262bc434d616dd1bb3bb7a8a3637
Bob <54476467+BobbyMcdonald@users.noreply.github.com>
2020-11-23 10:05:46 +0100

    Tests

--- Diabetes/APIDataAccess.Tests/PermissionAccessTests.cs
+++ Diabetes/APIDataAccess.Tests/PermissionAccessTests.cs
@@ -1,5 +1,6 @@
 ﻿using APIDataAccess.DataAccess;
 using APIDataAccess.Internal.DataAccess;
+using APIDataAccess.Models.Permission;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 using System;
@@ -9,13 +10,33 @@ using System.Text;
 namespace APIDataAccess.Tests
 {
     [TestClass]
-    class PermissionAccessTests
+    public class PermissionAccessTests
     {
         [TestMethod]
         public void Get_Exists()
         {
             var sql = Substitute.For<ISqlDataAccess>();
+            sql.LoadData<PermissionModel, dynamic>("spPermission_Get", Arg.Any<object>(), "DDB").
+                Returns(new List<PermissionModel>() { new PermissionModel() });
             var data = new PermissionAccess(sql);
+
+            var res = data.Get(2);
+
+            Assert.IsTrue(res != null);
+        }
+
+        [TestMethod]
+        public void Get_NotExists()
+        {
+            var sql = Substitute.For<ISqlDataAccess>();
+            sql.LoadData<PermissionModel, dynamic>("spPermission_Get", Arg.Any<object>(), "DDB").
+                Returns(new List<PermissionModel>());
+
+            var data = new PermissionAccess(sql);
+
+            var res = data.Get(2);
+
+            Assert.AreEqual(null, res);
         }
 
     }
