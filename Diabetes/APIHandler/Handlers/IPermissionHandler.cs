8441750675d3b699759c7e92d04175546d951da7
Bob <54476467+BobbyMcdonald@users.noreply.github.com>
2020-11-23 12:40:22 +0100

    Fix

--- Diabetes/APIHandler/Handlers/IPermissionHandler.cs
+++ Diabetes/APIHandler/Handlers/IPermissionHandler.cs
@@ -9,7 +9,8 @@ namespace APIHandler.Handlers
         void Delete(int id);
         void DeleteByUserId(string userId);
         PermissionDBModel Get(int id);
-        List<PermissionDBModel> GetByUserId(string userId);
+        List<PermissionDBModel> GetByTargetId(string targetId);
+        List<PermissionDBModel> GetByWatcherId(string watcherId);
         List<PermissionDBModel> GetPendingPermissions();
         int GetPermissionActtributes(List<PermissionDBModel> permissions);
         void Update(UpdatePermissionDBModel updatedPermission);
---
+++ Diabetes/DiabetesDB/dbo/Store Procedures/spPermission_UpdatePermissionModel.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[spPermission_UpdatePermissionModel]
+	@id nvarchar(450),
+	@StartDate datetime2(7), 
+	@ExpireDate datetime2(7),
+	@Days int,
+	@WeeksActive int,
+	@WeeksDeactive int,
+	@Attributes int
+AS
+BEGIN
+	UPDATE Permission
+	SET [StartDate] = @StartDate,
+	[ExpireDate] = @ExpireDate,
+	[Days] = @Days,
+	[WeeksActive] = @WeeksActive,
+	[WeeksDeactive] = @WeeksDeactive,
+	[Attributes] = @Attributes
+	WHERE [id] = @id; 	
+END
