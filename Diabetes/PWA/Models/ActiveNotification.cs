8338f3b3573cd51afc58f493cf0d2221ef18db5f
Theodor Risager <theodor349@gmail.com>
2020-11-26 13:21:44 +0100

    Notifications Implemented 50%

---
+++ Diabetes/PWA/Models/ActiveNotification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWA.Models
+{
+    public enum NotificationButtonType { Snooze, Dismiss}
+    public class ActiveNotification
+    {
+        public ActiveNotification()
+        {
+            Active = true;
+        }
+
+        public Subject Subject { get; set; }
+        public NotificationData Data { get; set; }
+        public bool Active { get; set; }
+
+        public override string ToString()
+        {
+            return Subject.ID + "-" + Data.Type.ToString();
+        }
+    }
+}
--- Diabetes/PWA/Models/NotificationData.cs
+++ Diabetes/PWA/Models/NotificationData.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 namespace PWA.Models
 {
     public enum NotificationType { Message, Warning }
-    public enum NotificationButtonType { Snooze, Dismiss}
     public enum ThresholdType { Low, High }
     public class NotificationData
     {
-        public string Name { get; set; }
         public NotificationType Type { get; set; }
         public string Title { get; set; }
         public string Note { get; set; }
