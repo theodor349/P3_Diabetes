568b3851bca38a77b0746f1023afa93be3382e74
Daniel Møller <57629796+Danielm0402@users.noreply.github.com>
2020-11-26 13:26:15 +0100

    NotificationSettingControllerTests fully implemented

--- Diabetes/APIDataAccess/Models/NotificationSetting/UpdateNotificationSettingModel.cs
+++ Diabetes/APIDataAccess/Models/NotificationSetting/UpdateNotificationSettingModel.cs
@@ -7,6 +7,16 @@ namespace APIDataAccess.Models.NotificationSetting
 {
     public class UpdateNotificationSettingModel
     {
+        public UpdateNotificationSettingModel()
+        {
+
+        }
+
+        public UpdateNotificationSettingModel(string note)
+        {
+            Note = note;
+        }
+
         public int ID { get; set; }
         public int ThresHold { get; set; }
         public ThresholdType ThresholdType { get; set; }
