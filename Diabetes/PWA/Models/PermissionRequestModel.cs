64cc37596861d79a0843374e418e36ce53cb5c85
Theodor Risager <theodor349@gmail.com>
2020-11-27 14:54:05 +0100

    Permission Requests Hooked up

--- Diabetes/PWA/Models/PermissionRequestModel.cs
+++ Diabetes/PWA/Models/PermissionRequestModel.cs
@@ -5,14 +5,11 @@ using System.Threading.Tasks;
 
 namespace PWA.Models
 {
-    public class LoginUser
+    public class PermissionRequestModel
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string NSLink { get; set; }
-        public string UserID { get; set; }
-        public string Token { get; set; }
 
         public string GetName()
         {
