8684f81031dfe1aea9f67fd98ed348655f51cbb5
Theodor Risager <theodor349@gmail.com>
2020-11-30 13:30:22 +0100

    Manage Connections

---
+++ Diabetes/PWA/Models/Permission.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWA.Models
+{
+    public class Permission : IComparable<Permission>
+    {
+        public int Id { get; set; }
+        public string TargetId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOwner { get; set; }
+        public bool BloodGlucose { get; set; }
+        public bool Battery { get; set; }
+        public bool Insulin { get; set; }
+        public DateTime ExpireDate { get; set; }
+
+        public string GetExpireDate()
+        {
+            return ExpireDate.ToString("g", CultureInfo.CreateSpecificCulture("es-ES"));
+        }
+
+        public string GetAccessString()
+        {
+            string s = "";
+            if (BloodGlucose)
+                s += "Bloodsugar";
+
+            if (s.Length > 0 && Battery)
+                s += ", Battery";
+            else if (Battery)
+                s += "Battery";
+
+            if (s.Length > 0 && Insulin)
+                s += ", Insulin";
+            else if (Insulin)
+                s += "Insuling";
+            return s;
+        }
+
+        public string GetName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public int CompareTo(Permission other)
+        {
+            return TargetId.CompareTo(other.TargetId);
+        }
+    }
+}
--- Diabetes/PWA/Models/Subject.cs
+++ Diabetes/PWA/Models/Subject.cs
@@ -10,10 +10,6 @@ namespace PWA.Models
         public string ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string NSLink { get; set; }
-        public bool IsEUMeasure { get; set; }
         public PumpData PumpData { get; set; }
         public List<NotificationData> NotificationDatas { get; set; }
 
