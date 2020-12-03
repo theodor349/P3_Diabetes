39437871d1d4a7e0f8e5afce91b25b04b9c07e86
Theodor Risager <theodor349@gmail.com>
2020-12-03 13:39:21 +0100

    Removed unessesary HttpCLient Injects

---
+++ Diabetes/PWA/Network/NetworkHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PWA.Network
+{
+    public class NetworkHelper
+    {
+        private readonly HttpClient _client;
+
+        public NetworkHelper(HttpClient client)
+        {
+            _client = client;
+        }
+
+
+    }
+}
--- Diabetes/PWA/Program.cs
+++ Diabetes/PWA/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PWA.Network;
 
 namespace PWA
 {
@@ -17,7 +18,9 @@ namespace PWA
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var client = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+            builder.Services.AddScoped(sp => client);
+            builder.Services.AddScoped(sp => new NetworkHelper(client));
 
             await builder.Build().RunAsync();
         }
